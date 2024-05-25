c0635c6c315a3dd841e296e143cbac4c43e8feb4
ngothankphuong <ngothanhphuong2703@gmail.com>
2024-05-25 21:35:49 +0700

    chưa sửa gì

---
+++ live_chat/client.py
@@ -0,0 +1,16 @@
+import socket
+
+clisocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+clisocket.connect(('localhost', 9999))
+print('The client has been connected')
+
+print('The message from server:', clisocket.recv(256).decode())
+
+print('Please keep providing the messages to send to server...')
+
+while True:
+    msg = input('Please write the message (string in quotes):')
+    clisocket.send(msg.encode('utf-8'))
+    response = clisocket.recv(256).decode()
+    print(f'Server response: {response}')
