05428e7b6af8e9c88a4323449883c53d414966a8
TuanLoc1006 <tuanlocjob@gmail.com>
2024-06-24 08:44:54 +0700

    add nlu

--- rasa_php/actions/action_dang_bo.py
+++ rasa_php/actions/action_dang_bo.py
@@ -97,14 +97,44 @@ class ActionLanhDaoDangDoanThe(Action):
         return "action_lanh_dao_dang_doan_the"
     
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_input = tracker.latest_message['text']
+        print("người dùng hỏi về lãnh đạo: " + user_input)
         # Lấy entities từ câu chat của người dùng
         entities = tracker.latest_message.get('entities', [])
         print(f"Entities từ input: {entities}")
         
         # Tạo dictionary ánh xạ các role với thông điệp tương ứng
         role_messages = {
-            'lãnh đạo': "</br>PGS.TS. BS. Nguyễn Minh Phương, Bí thư Đảng ủy, Email: nmphuong@ctump.edu.vn</br>TS. BS. Trần Thanh Hùng, Chủ tịch công đoàn Trường, Email: tthung@ctump.edu.vn</br>TS. BS. Phạm Hoàng Khánh, Bí thư Đoàn TNCSHCM Trường, Email: phkhanh@ctump.edu.vn</br>PGS.TS. BS. Lê Thành Tài, Chủ tịch Hội cựu chiến binh, Email: lttai@ctump.edu.vn</br>",
+            'hội đồng trường':'PGS.TS. BS. Nguyễn Minh Phương, Chủ tịch, Email: nmphuong@ctump.edu.vn',
+            'đảng và các đoàn thể': "</br>PGS.TS. BS. Nguyễn Minh Phương, Bí thư Đảng ủy, Email: nmphuong@ctump.edu.vn</br>TS. BS. Trần Thanh Hùng, Chủ tịch công đoàn Trường, Email: tthung@ctump.edu.vn</br>TS. BS. Phạm Hoàng Khánh, Bí thư Đoàn TNCSHCM Trường, Email: phkhanh@ctump.edu.vn</br>PGS.TS. BS. Lê Thành Tài, Chủ tịch Hội cựu chiến binh, Email: lttai@ctump.edu.vn</br>",
+            "khoa y":"Khoa Y</br>TS.BS. Lê Văn Minh, Trưởng Khoa, Email: lvminh@ctump.edu.vn</br>TS.BS. Trần Thái Thanh Tâm, Phó Trưởng Khoa, Email: ttttam@ctump.edu.vn</br>TS. BS. Nguyễn Như Nghĩa, Phó Trưởng Khoa, Email: nnnghia@ctump.edu.vn</br>",
+            "khoa dược":"Khoa Dược</br>PGS.TS. DS Phạm Thành Suôl, Trưởng Khoa, Email: ptsuol@ctump.edu.vn</br>PGS.TS.DS. Đỗ Châu Minh Vĩnh Thọ, Phó Trưởng Khoa, email: dcmvtho@ctump.edu.vn</br>TS. DS Phạm Thị Tố Liên, Phó Trưởng Khoa, Email: pttlien@ctump.edu.vn</br>",
+            "khoa răng hàm mặt":"Khoa Răng hàm mặt</br>PGS.TS. BS Trương Nhựt Khuê, Trưởng Khoa, Email: tnkhue@ctump.edu.vn</br>PGS.TS.BS. Lê Nguyên Lâm, Phó Trưởng Khoa, Email: lnlam@ctump.edu.vn</br>TS. BS. Đỗ Thị Thảo, Phó Trưởng Khoa, Email: dtthao@ctump.edu.vn</br>",
+            "khoa y tế công cộng":"Khoa Y Tế Công Cộng</br>TS. BS. Lê Minh Hữu, Trưởng Khoa , Email: lmhuu@ctump.edu.vn</br>TS. BS. Nguyễn Tấn Đạt, Phó Trưởng Khoa, Email: ntdat@ctump.edu.vn</br>",
+            "khoa điều dưỡng":"Khoa Điều dưỡng</br>TS. BS Nguyễn Hồng Phong, Trưởng Khoa , Email: nhphong@ctump.edu.vn</br>TS. ĐD. Nguyễn Thị Thanh Trúc, Phó Trưởng Khoa, Email: ntttruc@ctump.edu.vn</br>",
+            "khoa kỹ thuật y học":"Khoa Kỹ thuật y học</br>TS. BS Nguyễn Hồng Phong, Trưởng Khoa , Email: nhphong@ctump.edu.vn</br>TS. ĐD. Nguyễn Thị Thanh Trúc, Phó Trưởng Khoa, Email: ntttruc@ctump.edu.vn</br>",
+            "khoa học cơ bản":"Khoa học cơ bản</br>PGS.TS. Nguyễn Thị Thu Trâm, Trưởng Khoa, Email: ntttram@ctump.edu.vn</br>ThS. Nguyễn Thanh Hùng, Phó Trưởng Khoa, Email: nthung@ctump.edu.vn</br>TS. Trần Thị Hồng Lê, Phó Trưởng Khoa, Email: tthle@ctump.edu.vn</br>",
+            "khoa y học cổ truyền":"Khoa Y học cổ truyền</br>TS. BS. Lê Minh Hoàng , Trưởng Khoa, Email: lmhoang@ctump.edu.vn</br>ThS. BS. Nguyễn Ngọc Chi Lan , Phó Trưởng  Khoa, Email: nnclan@ctump.edu.vn</br>",
             
+            "phòng thông tin và truyền thông":"Phòng Thông tin và Truyền thông</br>ThS. Châu Minh Khoa, Trưởng Phòng, Email: cmkhoa@ctump.edu.vn</br>ThS. Trần Thị Bích Phương, Phó Trưởng Phòng, Email: ttbphuong@ctump.edu.vn</br>",
+            "phòng công tác sinh viên":"Phòng Công tác sinh viên</br>ThS. Ngô Phương Thảo, Trưởng phòng, Email: npthao@ctump.edu.vn</br>ThS. Võ Văn Quyền, Phó Trưởng phòng, Email: vvquyen@ctump.edu.vn</br>",
+            "phòng đảm bảo chất lượng":"Phòng đảm bảo chất lượng</br>ThS. BS. Phạm Thị Mỹ Ngọc, Trưởng Phòng, Email: ptmngoc@ctump.edu.vn</br>",
+            "phòng đào tạo đại học":"Phòng Đào tạo Đại học</br>TS. BS. Phạm Kiều Anh Thơ, Trưởng phòng, Email: pkatho@ctump.edu.vn</br>TS. BS. Trần Quang Khải, Phó Trưởng phòng, Email: tqkhai@ctump.edu.vn</br>",
+            "phòng đào tạo sau đại học":"Phòng Đào tạo Sau đại học</br>GS.TS. BS Võ Huỳnh Trang, Trưởng phòng, Email: vhtrang@ctump.edu.vn</br>TS. BS. Nguyễn Hồng Hà, Phó Trưởng phòng, Email: nhha@ctump.edu.vn</br>TS. DS Đặng Duy Khánh, Phó Trưởng phòng , Email: ddkhanh@ctump.edu.vn</br>",
+            "phòng hành chính tổng hợp":"Phòng Hành chính tổng hợp</br>ThS. Phạm Trương Yến Nhi , Trưởng phòng, Email: ptynhi@ctump.edu.vn</br>ThS. Phạm Thị Minh, Phó Trưởng phòng, Email: ptminh@ctump.edu.vn</br>CN. Nguyễn Văn Bộ, Phó Trưởng phòng, Email: nvbo@ctump.edu.vn</br>",
+            "phòng khảo thi":"Phòng Khảo thí</br>ThS. Phan Thị Tuyết Nhung, Trưởng Phòng, Email: pttnhung@ctump.edu.vn</br>Ths. DS. Lê Thị Minh Ngọc, Phó Trưởng Phòng, Email: ltmngoc@ctump.edu.vn</br>",
+            "phòng khoa học công nghệ và quan hệ đối ngoại":"Phòng Khoa học công nghệ và Quan hệ đối ngoại</br>PGS. TS. DS. Nguyễn Thị Ngọc Vân, Trưởng phòng, Email:  ntnvan@ctump.edu.vn</br>PGS.TS. DS. Nguyễn Thắng, Phó Trưởng phòng, Email: nthang@ctump.edu.vn</br>TS. BS. Hoàng Minh Tú, Phó Trưởng phòng, Email: hmtu@ctump.edu.vn</br>",
+            "phòng quản trị thiết bị":"Phòng Quản trị thiết bị</br>ThS. Nguyễn Văn Tám, Trưởng phòng, Email: nvtam@ctump.edu.vn</br>ThS. Huỳnh Trường Hiệp, Phó Trưởng phòng, Email: hthiep@ctump.edu.vn</br>",
+            "phòng tài chính kế toán":"Phòng Tài chính kế toán</br>ThS. Võ Nhật Nhân Tuyền, Trưởng phòng, Email: vnntuyen@ctump.edu.vn</br>CN. Hứa Kim Chi, Phó Trưởng phòng, Email: hkchi@ctump.edu.vn</br>",
+            "phòng tổ chức cán bộ":"Phòng Tổ chức Cán bộ</br>ThS. Trần Trương Ngọc Bích, Trưởng Phòng, Email:  ttnbich@ctump.edu.vn</br>ThS. Hà Bảo Trân, Phó Trưởng phòng, Email: hbtran@ctump.edu.vn</br>",
+            "phòng thanh tra pháp chế":"Phòng Thanh tra - Pháp chế</br>TS. BS. Bùi Quang Nghĩa, Trưởng Phòng, Email: bqnghia@ctump.edu.vn</br>CN. Nguyễn Hiệp Phúc, Phó Trưởng phòng, Email: nhphuc@ctump.edu.vn</br>",
+            "ban quản lý dự án":"Ban Quản lý dự án</br>KS. Nguyễn Chí Trung, Phó Trưởng Ban, Email: nctrung@ctump.edu.vn</br>",
+            "trung tâm dịch vụ và đào tạo theo nhu cầu xã hội":"Trung tâm Dịch vụ và Đào tạo theo nhu cầu xã hội</br>TS. BS. Nguyễn Triều Việt, Giám đốc, Email: ntviet@ctump.edu.vn</br>TS. Phan Thị Luyện, Phó Giám đốc, Email: ptluyen@ctump.edu.vn</br>",
+            "trung tâm giáo dục y học và huấn luyện kỹ năng y khoa":"Trung tâm Giáo dục y học và Huấn luyện kỹ năng y khoa</br>ThS. BS Phạm Thị Mỹ Ngọc, Trưởng Trung tâm, Email: ptmngoc@ctump.edu.vn</br>ThS. BS. Trần Xuân Quỳnh, Phó Trưởng Trung tâm, Email: txquynh@ctump.edu.vn</br>",
+            "thư viện":"Thư viện</br>TS. BS. Nguyễn Thị Hải Yến, Giám đốc, Email: nthyen@ctump.edu.vn</br>TS.BS. Lê Thị Hoàng Mỹ , Phó Giám đốc, Email: lthmy@ctump.edu.vn</br>",
+            "bệnh viện":"BỆNH VIỆN</br>BSCKII. Lại Văn Nông, Giám đốc, Email: lvnong@ctump.edu.vn.</br>PGS. TS.BS. Nguyễn Thành Tấn, Phó Giám đốc, Email: nttan@ctump.edu.vn</br>ThS. Nguyễn Văn Tám, Phó Giám đốc, Email: nvtam@ctump.edu.vn</br>TS. BS. Võ Phạm Minh Thư, Phó Giám đốc, Email: vpmthu@ctump.edu.vn</br>"
+            
+
         }
         
         message = ""
@@ -119,5 +149,7 @@ class ActionLanhDaoDangDoanThe(Action):
             dispatcher.utter_message(text=message)
         else:
             dispatcher.utter_message(text="Không tìm thấy thông tin phù hợp.")
+            file_writer = write_file()
+            file_writer.get_ghi_log_file('action_cap_lai_thanh_phi_bao_hiem_tai_nan: '+user_input)
         
         return []
--- rasa_php/actions/action_thong_tin_truong.py
+++ rasa_php/actions/action_thong_tin_truong.py
@@ -16,52 +16,119 @@ from datetime import datetime
 logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 
-class actionThongTinTruong(Action):
-    def name(self) -> Text:
-        return "action_thong_tin_truong"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        universityy_entity = next(tracker.get_latest_entity_values('university'), None)
-        user_input = tracker.latest_message['text']
-        print("người dùng hỏi thông tin trường: "+user_input)
-        logging.info("{}{}".format('Call action_thong_tin_truong: ', universityy_entity))
-        # Tạo từ điển các từ liên quan đến "Đại học Y Dược Cần Thơ"
-        related_terms = {
-            "ctump", 
-            "đại học y dược cần thơ", 
-            "đại học y",
-            "y dược cần thơ", 
-            "trường này", 
-            "trường",
-            "ctu medical and pharmaceutical university",
-            "can tho university of medicine and pharmacy",
-            "dhydct",
-            "dh y duoc can tho",
-            "can tho y duoc",
-            "can tho y duoc university",
-            "trường đại học y dược",
-            "đại học y cần thơ",
-            "đại học y dược ct",
-            "y dược ct",
-            "thông tin trường này",
-            "thông tin trường",
-            
+
+class actionThongTinTruong(Action):
+    def name(self):
+        return "action_thong_tin_truong"
+    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Lấy entities từ câu chat của người dùng
+        entities = tracker.latest_message.get('entities', [])
+        print(f"\nEntities từ input:\n {entities}")
+        
+        # Tạo dictionary ánh xạ các role với thông điệp tương ứng
+        role_messages = {
+            "thông tin": "Trường Đại học Y Dược Cần Thơ là trường đại học Khoa học Sức khỏe hệ công lập, trực thuộc Bộ Y tế duy nhất tại Đồng bằng sông Cửu Long, Việt Nam, Trường Đại học Y Dược Cần Thơ cũng là một trong những trường đào tạo Y Dược tốt nhất Việt Nam nói chung và khu vực Đồng bằng Sông Cửu Long nói riêng.",
+            "lịch sử":"Lịch sử hình thành và phát triển trường đại học y dược cần thơ http://www.ctump.edu.vn/Default.aspx?tabid=2048"
         }
         
-        if universityy_entity and universityy_entity.lower() in related_terms:
-            # Trả lời thông tin về trường Đại học Y Dược Cần Thơ
-            dispatcher.utter_message(
-               
-                text=f"Trường Đại học Y Dược Cần Thơ là trường đại học Khoa học Sức khỏe hệ công lập, trực thuộc Bộ Y tế duy nhất tại Đồng bằng sông Cửu Long, Việt Nam, Trường Đại học Y Dược Cần Thơ cũng là một trong những trường đào tạo Y Dược tốt nhất Việt Nam nói chung và khu vực Đồng bằng Sông Cửu Long nói riêng. "
-            )
-        else:
-            # Trả lời rằng không có thông tin về trường này
-            dispatcher.utter_message(
-                text="Xin lỗi, hiện tại chưa có thông tin về trường bạn yêu cầu."
-            )
-            file_writer = write_file()
-            file_writer.get_ghi_log_file('Action học phí: '+user_input)
+        message = ""
         
+        # Phân loại các role từ entities và thêm thông điệp tương ứng
+        for entity in entities:
+            role = entity.get('role')
+            if role in role_messages:
+                message = role_messages[role]
+        
+        if message:
+            dispatcher.utter_message(text=message)
+        else:
+            dispatcher.utter_message(text="Xin lỗi, hiện tại chưa có thông tin về trường bạn yêu cầu.")
+       
+        return []
+# class actionThongTinTruong(Action):
+#     def name(self) -> Text:
+#         return "action_thong_tin_truong"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         universityy_entity = next(tracker.get_latest_entity_values('university'), None)
+#         user_input = tracker.latest_message['text']
+#         print("người dùng hỏi thông tin trường: "+user_input)
+#         logging.info("{}{}".format('Call action_thong_tin_truong: ', universityy_entity))
+#         # Tạo từ điển các từ liên quan đến "Đại học Y Dược Cần Thơ"
+#         related_terms = {
+#             "ctump", 
+#             "đại học y dược cần thơ", 
+#             "đại học y",
+#             "y dược cần thơ", 
+#             "trường này", 
+#             "trường",
+#             "ctu medical and pharmaceutical university",
+#             "can tho university of medicine and pharmacy",
+#             "dhydct",
+#             "dh y duoc can tho",
+#             "can tho y duoc",
+#             "can tho y duoc university",
+#             "trường đại học y dược",
+#             "đại học y cần thơ",
+#             "đại học y dược ct",
+#             "y dược ct",
+#             "thông tin trường này",
+#             "thông tin trường",
+            
+#         }
+        
+#         if universityy_entity and universityy_entity.lower() in related_terms:
+#             # Trả lời thông tin về trường Đại học Y Dược Cần Thơ
+#             dispatcher.utter_message(
+               
+#                 text=f"Trường Đại học Y Dược Cần Thơ là trường đại học Khoa học Sức khỏe hệ công lập, trực thuộc Bộ Y tế duy nhất tại Đồng bằng sông Cửu Long, Việt Nam, Trường Đại học Y Dược Cần Thơ cũng là một trong những trường đào tạo Y Dược tốt nhất Việt Nam nói chung và khu vực Đồng bằng Sông Cửu Long nói riêng. "
+#             )
+#         else:
+#             # Trả lời rằng không có thông tin về trường này
+#             dispatcher.utter_message(
+#                 text="Xin lỗi, hiện tại chưa có thông tin về trường bạn yêu cầu."
+#             )
+#             file_writer = write_file()
+#             file_writer.get_ghi_log_file('Action học phí: '+user_input)
+        
+#         return []
+    
+# /////////////////////////////
+
+class actionSuMangTamNhinTrietLyGiaoDuc(Action):
+    def name(self):
+        return "action_sm_tn_gtcl_tlgd"
+    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Lấy entities từ câu chat của người dùng
+        entities = tracker.latest_message.get('entities', [])
+        print(f"\nEntities từ input:\n {entities}")
+        
+        # Tạo dictionary ánh xạ các role với thông điệp tương ứng
+        role_messages = {
+            "sứ mạng": "Sứ mạng: Trường Đại học Y Dược Cần Thơ có sứ mạng Đào tạo nguồn nhân lực y tế chất lượng cao; nghiên cứu khoa học, ứng dụng và chuyển giao công nghệ; bảo vệ, chăm sóc và nâng cao sức khỏe Nhân dân.</br>",
+            "tầm nhìn": "Tầm nhìn: Đến năm 2025: Là một trong 05 trường đại học khoa học sức khỏe hàng đầu Việt Nam và xếp hạng trong 500 trường đại học hàng đầu Đông Nam Á.Đến năm 2030: Là một trong 05 trường đại học khoa học sức khỏe hàng đầu Việt Nam và xếp hạng trong 1000 trường đại học hàng đầu Châu Á.</br>",
+            "giá trị cốt lỗi":"Giá trị cốt lỗi: Trách nhiệm - Chất lượng - Phát triển - Hội nhập</br>",
+            "triết lý giáo dục":"Triết lý giáo dục: Trí tuệ - Y đức - Sáng tạo</br>",
+            "mục tiêu":"I. Mục tiêu chung</br>Trường Đại học Y Dược Cần Thơ là trường đại học trọng điểm quốc gia và xếp hạng trong 500 trường đại học hàng đầu Đông Nam Á.</br>II. Mục tiêu cụ thể</br>1) Đảm bảo tiêu chuẩn đầu vào và đầu ra chất lượng cao.</br>2) Đảm bảo sự hài lòng của các bên liên quan.</br>3) Đa dạng hóa loại hình đào tạo, chương trình đào tạo và dịch vụ.</br>4) Nâng cao hiệu quả ứng dụng khoa học công nghệ.</br>5) Quản lý và khai thác hiệu quả thông tin dữ liệu quản trị Trường.</br>6) Sử dụng công nghệ số trong vận hành các hoạt động của Trường.</br>7) Phát triển nguồn nhân lực đáp ứng yêu cầu hội nhập.</br>8) Cải tiến liên tục mọi hoạt động của Trường.</br>9) Đổi mới phương thức quản trị, nâng cao văn hóa tổ chức.</br>10) Đa dạng hóa nguồn thu, tối ưu hóa chi phí hoạt động và nâng cao hiệu quả.</br>11) Phát triển Bệnh viện trường thành bệnh viện hiện đại, kỹ thuật cao của khu vực.</br>",
+            "slogan":"Khẩu hiệu của trường: Tri thức cho bạn - sức khỏe cho đời"
+        }
+        
+        message = ""
+        
+       
+        for entity in entities:
+            role = entity.get('role')
+            if role in role_messages:
+                message += role_messages[role]
+        
+        if message:
+            dispatcher.utter_message(text=message)
+        else:
+            dispatcher.utter_message(text="Xin lỗi, hiện tại chưa có thông tin bạn yêu cầu.")
+       
         return []
